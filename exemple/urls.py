49822ce3f5834e170b1a0fdb3bed3a621a90127f
Vania Kazeko <vania.kazekj.00@bk.ry>
2022-10-18 12:51:45 +0300

    add about

--- exemple/urls.py
+++ exemple/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include('portfolio.urls')),
     path('', include('blog.urls')),
     path('', include('services.urls')),
-
+    path('', include('about.urls')),
 
     path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
