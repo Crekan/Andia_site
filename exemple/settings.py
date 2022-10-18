a09803181e9190289d75e87800463f1846c488c1
Vania Kazeko <vania.kazekj.00@bk.ry>
2022-10-18 12:15:08 +0300

    add servicer

--- exemple/settings.py
+++ exemple/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'about.apps.AboutConfig',
     'services.apps.ServicesConfig',
     'blog.apps.BlogConfig',
     'portfolio.apps.PortfolioConfig',
--- exemple/urls.py
+++ exemple/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('', include('services.urls')),
 
+
     path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
